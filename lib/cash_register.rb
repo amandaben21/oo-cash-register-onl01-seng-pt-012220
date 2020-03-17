bb36ae22a6e63415a12c88365809befdcb9b464c
Amanda Contreras <amandaben21@aol.com>
2020-03-17 20:53:42 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,7 +1,7 @@
 class CashRegister
-  attr_accessor :total, :discount
+  attr_accessor :total
   
-  def initialize(total = 0, discount)
+  def initialize(total = 0)
     @total = total
     @discount = discount
   end 
